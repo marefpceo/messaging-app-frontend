3a0083f015ec863aafc155d64cb0619f20c01909
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-06-20 16:56:06 -0700

    Created Login page shell with routing

--- src/utilities/Routes.jsx
+++ src/utilities/Routes.jsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import App from '../pages/App';
 import Signup from '../pages/Signup';
+import Login from '../pages/Login';
 
 function Router() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <App />,
-      children: [{ path: 'signup', element: <Signup /> }],
+      children: [
+        { path: 'signup', element: <Signup /> },
+        { path: 'login', element: <Login /> },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
