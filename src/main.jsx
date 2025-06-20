7735a482a6f7eaa8403fdc996dd54ff256b8b05a
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-06-20 16:16:57 -0700

    Configured tailwindcss and materialUI integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './utilities/Routes';
-import './index.css'
+
+import { StyledEngineProvider } from '@mui/material/styles';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
-  </React.StrictMode>
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles='@layer theme, base, mui, components, utilites;' />
+      <Router />
+    </StyledEngineProvider>
+  </React.StrictMode>,
 );
