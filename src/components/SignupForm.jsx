5adae13848933a5ca79d2da3f34d2c05ffc4eaaf
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-06-25 16:09:33 -0700

    Set up controlled inputs for Signup form

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -4,7 +4,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 
-function SignupForm() {
+function SignupForm({ userSignUp, handleChange }) {
   return (
     <Box
       component='div'
@@ -15,87 +15,102 @@ function SignupForm() {
     >
       <h1 className='text-2xl'>Sign up</h1>
 
-      <div className='inputDiv mt-4'>
-        <TextField
-          fullWidth
-          id='firstname'
-          name='firstname'
-          variant='outlined'
-          size='small'
-          margin='dense'
-          label='First Name'
-          color='success'
-        />
-        <TextField
-          fullWidth
-          id='lastname'
-          name='lastname'
-          variant='outlined'
-          size='small'
-          margin='dense'
-          label='Last Name'
-          color='success'
-        />
-        <TextField
-          fullWidth
-          id='email'
-          name='email'
-          variant='outlined'
-          size='small'
-          margin='dense'
-          label='Email'
-          type='email'
-          color='success'
-        />
-        <TextField
-          fullWidth
-          id='date_of_birth'
-          name='date_of_birth'
-          variant='outlined'
-          size='small'
-          margin='dense'
-          label='Date of Birth'
-          color='success'
-        />
-        <TextField
-          fullWidth
-          id='username'
-          name='username'
-          variant='outlined'
-          size='small'
-          margin='dense'
-          label='Username'
-          color='success'
-        />
-        <TextField
-          fullWidth
-          id='password'
-          name='password'
-          variant='outlined'
-          size='small'
-          margin='dense'
-          label='Password'
-          type='password'
-          color='success'
-        />
-        <TextField
-          fullWidth
-          id='matchPassword'
-          name='matchPassword'
-          variant='outlined'
-          size='small'
-          margin='dense'
-          label='Confirm Password'
-          type='password'
-          color='success'
-        />
-      </div>
-
-      <div className='buttonDiv mt-12 mb-6 flex flex-col justify-evenly'>
-        <Button variant='contained' color='primary'>
-          Sign up
-        </Button>
-      </div>
+      <form action=''>
+        <div className='inputDiv mt-4'>
+          <TextField
+            fullWidth
+            id='firstname'
+            name='firstname'
+            variant='outlined'
+            size='small'
+            margin='dense'
+            label='First Name'
+            color='success'
+            value={userSignUp.firstname}
+            onChange={handleChange}
+          />
+          <TextField
+            fullWidth
+            id='lastname'
+            name='lastname'
+            variant='outlined'
+            size='small'
+            margin='dense'
+            label='Last Name'
+            color='success'
+            value={userSignUp.lastname}
+            onChange={handleChange}
+          />
+          <TextField
+            fullWidth
+            id='email'
+            name='email'
+            variant='outlined'
+            size='small'
+            margin='dense'
+            label='Email'
+            type='email'
+            color='success'
+            value={userSignUp.email}
+            onChange={handleChange}
+          />
+          <TextField
+            fullWidth
+            id='date_of_birth'
+            name='date_of_birth'
+            variant='outlined'
+            size='small'
+            margin='dense'
+            label='Date of Birth'
+            color='success'
+            value={userSignUp.date_of_birth}
+            onChange={handleChange}
+          />
+          <TextField
+            fullWidth
+            id='username'
+            name='username'
+            variant='outlined'
+            size='small'
+            margin='dense'
+            label='Username'
+            color='success'
+            value={userSignUp.username}
+            onChange={handleChange}
+          />
+          <TextField
+            fullWidth
+            id='password'
+            name='password'
+            variant='outlined'
+            size='small'
+            margin='dense'
+            label='Password'
+            type='password'
+            color='success'
+            value={userSignUp.password}
+            onChange={handleChange}
+          />
+          <TextField
+            fullWidth
+            id='matchPassword'
+            name='matchPassword'
+            variant='outlined'
+            size='small'
+            margin='dense'
+            label='Confirm Password'
+            type='password'
+            color='success'
+            value={userSignUp.matchPassword}
+            onChange={handleChange}
+          />
+        </div>
+        <div className='buttonDiv mt-12 mb-6 flex flex-col justify-evenly'>
+          <Button variant='contained' color='primary'>
+            Sign up
+          </Button>
+        </div>
+      </form>
 
       <Divider>or</Divider>
 
--- src/sections/Signup.jsx
+++ src/sections/Signup.jsx
@@ -1,14 +1,34 @@
-import { Navigate } from 'react-router';
+import { Link, Navigate } from 'react-router';
 import SignupForm from '../components/SignupForm';
+import { useState } from 'react';
 
 function Signup({ isLoggedIn }) {
+  const [userSignUp, setUserSignUp] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    date_of_birth: '',
+    username: '',
+    password: '',
+    matchPassword: '',
+  });
+
   if (isLoggedIn) {
     return <Navigate to={'/user'} replace />;
   }
 
+  function handleChange(e) {
+    const value = e.target.value;
+    setUserSignUp((prevData) => ({
+      ...prevData,
+      [e.target.name]: value,
+    }));
+    console.log(userSignUp);
+  }
+
   return (
     <section className='mt-10 flex flex-col flex-1 items-center justify-start'>
-      <SignupForm />
+      <SignupForm userSignUp={userSignUp} handleChange={handleChange} />
     </section>
   );
 }
