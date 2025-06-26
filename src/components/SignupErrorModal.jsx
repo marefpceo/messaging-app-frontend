eca372e3608be9a0ea413c4d2c8e5a48f177251e
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-06-26 16:04:04 -0700

    Created boilerplate for Signup Error Modal

---
+++ src/components/SignupErrorModal.jsx
@@ -0,0 +1,14 @@
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+
+function SignupErrorModal() {
+  return (
+    <>
+      <h2>signup error modal</h2>
+    </>
+  );
+}
+
+export default SignupErrorModal;
