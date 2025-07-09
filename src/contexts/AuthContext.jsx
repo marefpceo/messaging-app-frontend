7848322e5fc476ba4d91bdb74d365791136568d0
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-07-09 13:41:45 -0700

    Added verifySession to check session

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,3 +1,39 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-export const AuthContext = createContext(false);
+const AuthContext = createContext(false);
+
+function AuthProvider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    async function verifySession() {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/session-status`,
+        {
+          credentials: 'include',
+        },
+      );
+
+      const responseData = await response.json();
+
+      if (responseData.status === 'active') {
+        console.log(responseData);
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    }
+    verifySession();
+
+    console.log('Test effect AuthContext');
+    console.log(isAuthenticated);
+  });
+
+  return (
+    <AuthContext value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext>
+  );
+}
+
+export { AuthContext, AuthProvider };
