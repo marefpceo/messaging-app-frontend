f1647b8e86bf818084c115cdc18e28413e91c78a
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-07-09 14:03:54 -0700

    Updated ProtectedInterface rerouting

--- src/pages/ProtectedInterface.jsx
+++ src/pages/ProtectedInterface.jsx
@@ -1,9 +1,10 @@
-import { Outlet, Navigate } from 'react-router';
+import { Outlet, Navigate, useOutletContext, useLocation } from 'react-router';
 import { useContext, useState } from 'react';
 import { UserContext } from '../contexts/UserContext';
 import { AuthContext } from '../contexts/AuthContext';
 
 function ProtectedInterface() {
+  const location = useLocation();
   const userData = useContext(UserContext);
   const authStatus = useContext(AuthContext);
   const [user, setUser] = useState(userData.user, userData.setUser);
@@ -11,17 +12,16 @@ function ProtectedInterface() {
     authStatus.isAuthenticated,
     authStatus.setIsAuthenticated,
   );
+  const { logout } = useOutletContext();
 
-  if (!isAuthenticated) {
-    return <Navigate to={'/'} replace={true} />;
-  }
-
-  return (
+  return isAuthenticated ? (
     <section className='flex flex-col p-2 h-svh overflow-auto'>
       <Outlet
-        context={{ user, setUser, isAuthenticated, setIsAuthenticated }}
+        context={{ user, setUser, isAuthenticated, setIsAuthenticated, logout }}
       />
     </section>
+  ) : (
+    <Navigate to={'/login'} replace state={{ from: location }} />
   );
 }
 
