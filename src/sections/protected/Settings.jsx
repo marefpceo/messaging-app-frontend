2a1cbf77c215c85448417e4ce9a1edccb4a3f377
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-07-18 17:40:19 -0700

    Working on Settings section

--- src/sections/protected/Settings.jsx
+++ src/sections/protected/Settings.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import HomeNav from '../../components/HomeNav';
 import InterfaceHeader from '../../components/InterfaceHeader';
+import Divider from '@mui/material/Divider';
 
 function Settings() {
   const { user } = useContext(AuthContext);
@@ -11,9 +12,11 @@ function Settings() {
       <section className='flex flex-1 flex-col p-2 bg-slate-100'>
         <InterfaceHeader title={'Settings'} user={user} />
 
-        <div className='homeBody min-h-full flex flex-col justify-center items-center'>
+        <div className='user-profile h-full flex flex-col justify-center items-center'>
           <p>User Setting icons / or individual settings</p>
         </div>
+
+        <Divider className='bg-amber-700' />
       </section>
       <HomeNav />
     </>
