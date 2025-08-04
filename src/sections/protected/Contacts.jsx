8316ed867302e242f05752ceb116ac76ead2e315
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-08-04 08:49:25 -0700

    Updated Contacts section
    - Added api calls for user contact list and full contact list.
    
    Continue working on displaying data retrieved from API call.

--- src/sections/protected/Contacts.jsx
+++ src/sections/protected/Contacts.jsx
@@ -1,14 +1,52 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import HomeNav from '../../components/HomeNav';
 import InterfaceHeader from '../../components/InterfaceHeader';
 import ContactTabPanel from '../../components/ContactTabPanel';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const apiHeader = {
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
 
 function Contacts() {
   const { user } = useContext(AuthContext);
   const [value, setValue] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [contactList, setContactList] = useState([]);
+  const [userContacts, setUserContacts] = useState([]);
+
+  useEffect(() => {
+    async function getContacts() {
+      try {
+        const [fullList, userList] = await Promise.all([
+          fetch(`${import.meta.env.VITE_API_BASE_URL}/contact`, apiHeader),
+          fetch(
+            `${import.meta.env.VITE_API_BASE_URL}/contact/${user.id}/contact_list`,
+            apiHeader,
+          ),
+        ]);
+
+        const fullListData = await fullList.json();
+        const userListData = await userList.json();
+
+        if (fullList.ok && userList.ok) {
+          setContactList(fullListData);
+          setUserContacts(userListData);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getContacts();
+  }, [user.id]);
 
   function handleChange(e, newValue) {
     setValue(newValue);
@@ -16,26 +54,37 @@ function Contacts() {
 
   return (
     <>
-      <section
-        className={`flex flex-1 flex-col p-2 ${user.settings.background} ${user.settings.color} ${user.settings.font}`}
-      >
-        <InterfaceHeader title={'Contacts'} user={user} />
-
-        <div className='contact-tabs mt-4 flex justify-center'>
-          <Tabs value={value} onChange={handleChange} aria-label='contact tabs'>
-            <Tab label='My Contacts' />
-            <Tab label='All' />
-          </Tabs>
+      {isLoading ? (
+        <div className='flex flex-col flex-1 justify-center items-center'>
+          <CircularProgress />
+          <p>Loading</p>
         </div>
+      ) : (
+        <section
+          className={`flex flex-1 flex-col p-2 ${user.settings.background} ${user.settings.color} ${user.settings.font}`}
+        >
+          <InterfaceHeader title={'Contacts'} user={user} />
 
-        <ContactTabPanel value={value} index={0}>
-          List of my contacts
-        </ContactTabPanel>
+          <div className='contact-tabs mt-4 flex justify-center'>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label='contact tabs'
+            >
+              <Tab label='My Contacts' />
+              <Tab label='All' />
+            </Tabs>
+          </div>
 
-        <ContactTabPanel value={value} index={1}>
-          List of ALL contacts
-        </ContactTabPanel>
-      </section>
+          <ContactTabPanel value={value} index={0}>
+            List of my contacts
+          </ContactTabPanel>
+
+          <ContactTabPanel value={value} index={1}>
+            List of ALL contacts
+          </ContactTabPanel>
+        </section>
+      )}
       <HomeNav />
     </>
   );
