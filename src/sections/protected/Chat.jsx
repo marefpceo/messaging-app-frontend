9c1c6195b7f2ae71b8c8ff41a69877f47ad04294
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-08-25 13:30:15 -0700

    Added New Chat button with conditional
    - Added Chat button to create a new message.
    - Conditional statement added to determine whether or not to display
    button text based on media query screen size.

--- src/sections/protected/Chat.jsx
+++ src/sections/protected/Chat.jsx
@@ -1,20 +1,52 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import HomeNav from '../../components/HomeNav';
 import InterfaceHeader from '../../components/InterfaceHeader';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+const apiHeader = {
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
 
 function Chat() {
   const { user } = useContext(AuthContext);
+  const matches = useMediaQuery('(max-width:600px)');
+  const [messageList, setMessageList] = useState(null);
 
   return (
     <>
-      <section className='flex flex-col flex-1 p-2 bg-slate-100'>
+      <div className='flex flex-col flex-1 p-2 bg-slate-100'>
         <InterfaceHeader title={'Chat'} user={user} />
 
         <div className='homeBody min-h-full flex flex-col justify-center items-center'>
-          <p>Message main area</p>
+          {messageList === null ? <p>No Messages</p> : <p>Message List</p>}
         </div>
-      </section>
+      </div>
+
+      {matches ? (
+        <IconButton
+          aria-label='create new message'
+          className='absolute right-4 bottom-20 text-gray-700 border border-gray-200 text-md shadow-lg shadow-gray-600'
+        >
+          <ChatBubbleIcon className='text-lime-600 text-2xl' />
+        </IconButton>
+      ) : (
+        <Button
+          variant='outlined'
+          startIcon={<ChatBubbleIcon className='text-lime-600 text-2xl' />}
+          className='absolute right-4 bottom-20 text-gray-700 border-gray-300 text-md shadow shadow-gray-600'
+        >
+          New Chat
+        </Button>
+      )}
+
       <HomeNav />
     </>
   );
