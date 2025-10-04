7deb378bcf3e48fc8cb7e7a4fd3c976988b846a4
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-10-04 08:39:59 -0700

    Added ReplyIcon to Messages component

--- src/sections/protected/Messages.jsx
+++ src/sections/protected/Messages.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { useOutletContext } from 'react-router';
 import Avatar from '@mui/material/Avatar';
 import ReplyIcon from '@mui/icons-material/Reply';
+import IconButton from '@mui/material/IconButton';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 
 function Messages() {
   const [
@@ -20,20 +22,35 @@ function Messages() {
 
   console.log(selectedConversation);
   return (
-    <div className='mt-10 flex flex-col'>
-      {selectedConversation &&
-        selectedConversation.messages.map((message) => (
-          <div
-            key={message.id}
-            className={`my-2 mx-4 p-4 max-w-3/4 flex gap-5 items-center 
-            ${user.id === message.senderId ? senderStyle : recipientStyle} border border-black 
-            rounded-lg `}
-          >
-            <Avatar>{`${message.recipient.username[0].toUpperCase()}`}</Avatar>
-            <p>{message.context}</p>
-          </div>
-        ))}
-    </div>
+    <>
+      <div className='mt-10 flex flex-col'>
+        {selectedConversation &&
+          selectedConversation.messages.map((message) => (
+            <div
+              key={message.id}
+              className={`my-2 mx-4 p-4 max-w-3/4 flex gap-5 items-center 
+              ${user.id === message.senderId ? senderStyle : recipientStyle} border border-black 
+              rounded-lg `}
+            >
+              <Avatar>{`${message.recipient.username[0].toUpperCase()}`}</Avatar>
+              <p>{message.context}</p>
+            </div>
+          ))}
+      </div>
+      <IconButton
+        className='absolute right-4 bottom-20 text-gray-700 border border-gray-200 text-md 
+          shadow-lg shadow-gray-600'
+      >
+        <NavigateBeforeIcon />
+      </IconButton>
+
+      <IconButton
+        className='absolute right-4 bottom-20 text-gray-700 border border-gray-200 text-md 
+          shadow-lg shadow-gray-600'
+      >
+        <ReplyIcon className='text-lime-600 text-2xl' />
+      </IconButton>
+    </>
   );
 }
 
