c4f0c0434f4b25a6bcb653eb2f5feb3fc87acf28
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-10-12 10:28:34 -0700

    Added buttons with logic for message delete
    Added Delete and Cancel icons with conditionals to determine which
    buttons to show and hide based on showDeleteButtons state

--- src/sections/protected/Messages.jsx
+++ src/sections/protected/Messages.jsx
@@ -4,6 +4,11 @@ import Avatar from '@mui/material/Avatar';
 import ReplyIcon from '@mui/icons-material/Reply';
 import IconButton from '@mui/material/IconButton';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import Checkbox from '@mui/material/Checkbox';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ClearIcon from '@mui/icons-material/Clear';
 import { AuthContext } from '../../contexts/AuthContext';
 
 function Messages() {
@@ -11,46 +16,87 @@ function Messages() {
   const navigate = useNavigate();
   const [handleReplyClick, setSelectedConversationId, selectedConversation] =
     useOutletContext();
-  const senderStyle = 'self-start ';
-  const recipientStyle = 'self-end flex-row-reverse';
+  const senderStyle = 'justify-start ';
+  const recipientStyle = 'just-end flex-row-reverse';
+  const [showDeleteButtons, setShowDeleteButtons] = useState(false);
 
   function getAvatarLetter(usernameInput) {
     const firstInitial = usernameInput[0].capitalize;
   }
 
+  function handleDeleteClick() {
+    if (showDeleteButtons === false) {
+      setShowDeleteButtons(true);
+    } else {
+      console.log('delete messages');
+    }
+  }
+
+  function handleCancelDelete() {
+    setShowDeleteButtons(false);
+  }
+
   return (
     <>
-      <div className='mt-10 flex flex-col'>
+      <div className='message-div mt-10 flex flex-col'>
         {selectedConversation &&
           selectedConversation.messages.map((message) => (
             <div
               key={message.id}
-              className={`my-2 mx-4 p-4 max-w-3/4 flex gap-5 items-center 
-              ${user.id === message.senderId ? senderStyle : recipientStyle} border border-black 
-              rounded-lg `}
+              className={`flex ${user.id === message.senderId ? senderStyle : recipientStyle} 
+               ${user.id !== message.senderId && showDeleteButtons ? 'mr-12' : ''}`}
             >
-              <Avatar>{`${message.recipient.username[0].toUpperCase()}`}</Avatar>
-              <p>{message.context}</p>
+              <div
+                className={`my-2 mx-4 p-4 max-w-3/4 flex gap-5 items-center 
+                 border border-black ${user.id === message.senderId ? 'flex-row' : 'flex-row-reverse'}
+                rounded-lg `}
+              >
+                <Avatar>{`${message.recipient.username[0].toUpperCase()}`}</Avatar>
+                <p>{message.context}</p>
+              </div>
+              <div
+                className={`${!showDeleteButtons ? 'hidden' : ''} self-center absolute right-4`}
+              >
+                <Checkbox
+                  value={message.id}
+                  icon={<RadioButtonUncheckedIcon />}
+                  checkedIcon={<RadioButtonCheckedIcon color='error' />}
+                />
+              </div>
             </div>
           ))}
       </div>
       <div className='absolute bottom-20 flex self-center gap-8'>
         <IconButton
-          className='text-gray-700 border border-gray-200 text-md
-            shadow-lg shadow-gray-600'
+          className={`text-gray-700 border border-gray-200 text-md
+            shadow-lg shadow-gray-600 ${showDeleteButtons ? 'hidden' : ''}`}
           onClick={() => navigate(-1)}
         >
           <NavigateBeforeIcon className='text-lime-600 text-2xl' />
         </IconButton>
         <IconButton
-          className='text-gray-700 border border-gray-200 text-md
-            shadow-lg shadow-gray-600'
+          className={`text-gray-700 border border-gray-200 text-md
+            shadow-lg shadow-gray-600 ${showDeleteButtons ? 'hidden' : ''}`}
           onClick={() => {
             setSelectedConversationId(selectedConversation.id);
             handleReplyClick();
           }}
         >
           <ReplyIcon className='text-lime-600 text-2xl' />
+        </IconButton>
+        <IconButton
+          className='text-gray-700 border border-gray-200 text-md
+            shadow-lg shadow-gray-600'
+          onClick={handleDeleteClick}
+        >
+          <DeleteIcon color='error' />
+        </IconButton>
+        <IconButton
+          className={`text-gray-700 border border-gray-200 text-md
+            shadow-lg shadow-gray-600 ${showDeleteButtons ? '' : 'hidden'}`}
+          onClick={handleCancelDelete}
+        >
+          <ClearIcon />
         </IconButton>
       </div>
     </>
