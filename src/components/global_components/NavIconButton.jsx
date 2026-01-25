fc9b0978a307c47454b91dffe8ee43efa950a5bd
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-01-24 19:16:59 -0800

    Updated Home and replaced icons
    - replaced MUI icons with Font Awesome icons

--- src/components/global_components/NavIconButton.jsx
+++ src/components/global_components/NavIconButton.jsx
@@ -1,7 +1,7 @@
 function NavIconButton({ icon, text, handleClick }) {
   return (
     <div
-      className='p-8 border rounded-2xl max-w-2/3 bg-gray-200 shadow-lg'
+      className='flex flex-col items-center p-4 border rounded-2xl w-1/4 bg-gray-200 shadow-lg'
       onClick={handleClick}
     >
       <div>{icon}</div>
--- src/sections/protected/Home.jsx
+++ src/sections/protected/Home.jsx
@@ -3,10 +3,13 @@ import { useNavigate } from 'react-router';
 import { AuthContext } from '../../contexts/AuthContext';
 import InterfaceHeader from '../../components/global_components/InterfaceHeader';
 import NavIconButton from '../../components/global_components/NavIconButton';
-import GroupIcon from '@mui/icons-material/Group';
-import ChatIcon from '@mui/icons-material/Chat';
-import SettingsIcon from '@mui/icons-material/Settings';
-import LogoutIcon from '@mui/icons-material/Logout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faAddressBook,
+  faComments,
+  faGear,
+  faArrowRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
   const { user, logout } = useContext(AuthContext);
@@ -23,63 +26,65 @@ function Home() {
 
   return (
     <>
-      {/* TODO stuck on loading after creating new user */}
-      {!userSettings ? (
-        <p>Loading. . . </p>
-      ) : (
-        <section className={`flex flex-1 flex-col p-2 `}>
-          <InterfaceHeader
-            title={'Home'}
-            message={'Welcome back '}
-            c
-            user={user}
-          />
+      <section className={`flex flex-1 flex-col p-2 `}>
+        <InterfaceHeader
+          title={'Home'}
+          message={'Welcome back '}
+          c
+          user={user}
+        />
 
-          <div
-            className={`homeBody h-full flex flex-col justify-evenly items-center ${userSettings.background} ${userSettings.color} ${userSettings.font}`}
-          >
-            <div className='flex flex-wrap gap-8 justify-center'>
-              <NavIconButton
-                text={'Contacts'}
-                icon={
-                  <GroupIcon
-                    className={'text-gray-600'}
-                    sx={{ fontSize: 60 }}
-                  />
-                }
-                handleClick={() => navigate('/user/contacts')}
-              />
-              <NavIconButton
-                text={'Chat'}
-                icon={
-                  <ChatIcon className={'text-gray-600'} sx={{ fontSize: 60 }} />
-                }
-                handleClick={() => navigate('/user/chat')}
-              />
-              <NavIconButton
-                text={'Settings'}
-                icon={
-                  <SettingsIcon
-                    className={'text-gray-600'}
-                    sx={{ fontSize: 60 }}
-                  />
-                }
-                handleClick={() => navigate('/user/settings')}
-              />
-              <NavIconButton
-                text={'Logout'}
-                icon={
-                  <LogoutIcon
-                    className={'text-gray-600'}
-                    sx={{ fontSize: 60 }}
-                  />
-                }
-                handleClick={logout}
-              />
-            </div>
+        <div
+          className={`homeBody h-full flex flex-col justify-evenly items-center`}
+        >
+          <div className='flex flex-wrap gap-12 justify-center'>
+            <NavIconButton
+              text={'Contacts'}
+              icon={
+                <FontAwesomeIcon
+                  icon={faAddressBook}
+                  className='text-gray-600'
+                  size='2x'
+                />
+              }
+              handleClick={() => navigate('/user/contacts')}
+            />
+            <NavIconButton
+              text={'Chat'}
+              icon={
+                <FontAwesomeIcon
+                  icon={faComments}
+                  className={'text-gray-600'}
+                  size='2x'
+                />
+              }
+              handleClick={() => navigate('/user/chat')}
+            />
+            <NavIconButton
+              text={'Settings'}
+              icon={
+                <FontAwesomeIcon
+                  icon={faGear}
+                  className={'text-gray-600'}
+                  size='2x'
+                />
+              }
+              handleClick={() => navigate('/user/settings')}
+            />
+            <NavIconButton
+              text={'Logout'}
+              icon={
+                <FontAwesomeIcon
+                  icon={faArrowRightFromBracket}
+                  className={'text-gray-600'}
+                  size='2x'
+                />
+              }
+              handleClick={logout}
+            />
           </div>
-        </section>
-      )}
+        </div>
+      </section>
     </>
   );
 }
