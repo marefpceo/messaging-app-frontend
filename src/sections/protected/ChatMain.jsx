13d20b345854ff28a05202ee412f95c8114c58ef
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-02-01 08:31:36 -0800

    Created ChatMain component as a chat landing page
    - ChatMain lists the user's Favorites to quickly and easily send
    messages.

---
+++ src/sections/protected/ChatMain.jsx
@@ -0,0 +1,48 @@
+import { useState, useContext, useEffect } from 'react';
+import { Link } from 'react-router';
+import { AuthContext } from '../../contexts/AuthContext';
+import { userListService } from '../../api/apiContactServices/contactServices';
+import Avatar from '../../components/Avatar';
+
+function ChatMain() {
+  const { user } = useContext(AuthContext);
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    async function getUserList() {
+      try {
+        // Returns array objects for favorite users
+        const serviceResponse = await userListService(user.username);
+
+        const userListData = await serviceResponse.json();
+
+        if (serviceResponse.ok) {
+          setUserList(userListData);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getUserList();
+  }, [user.id]);
+  console.log(userList);
+  return (
+    <section className='h-full flex flex-col *:flex-1'>
+      <div className='p-4'>
+        <h3 className='text-lg'>Favorites</h3>
+        <div className='flex flex-wrap justify-evenly text-black gap-3 mt-4'>
+          {userList.map((entry) => (
+            <Link to={`/user/chat/${entry.username}`} key={entry.id}>
+              <div className='mt-4 flex flex-col items-center'>
+                <Avatar />
+                <p className='mt-1'>{entry.username}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ChatMain;
