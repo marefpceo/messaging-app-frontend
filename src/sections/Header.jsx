7d51399ac0a4bd9db25b16bb3d43d57e2fba73d1
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-02-02 17:09:11 -0800

    Added meta description to Login component

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -2,14 +2,23 @@ import logoNameVertical from '../assets/mchat-logo-small.webp';
 
 function Header() {
   return (
-    <header className='m-4'>
-      <img
-        fetchPriority='high'
-        src={logoNameVertical}
-        alt='mChat logo'
-        width={160}
+    <>
+      <meta
+        name='description'
+        content={`Login to mChat Messenger to access your account and start
+        chatting with friends!`}
       />
-    </header>
+
+      <header className='m-4'>
+        <img
+          fetchPriority='high'
+          src={logoNameVertical}
+          alt='mChat logo'
+          width={160}
+          height={60}
+        />
+      </header>
+    </>
   );
 }
 
